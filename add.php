485c5ebbf9033849cf909c17d10e49734eee78cf
Md. Rakib Ul Hasan Sefat <58886202+rakibulhasan-111@users.noreply.github.com>
2021-11-25 11:53:10 +0600

    Add files via upload

--- add.php
+++ add.php
@@ -1,76 +1,91 @@
-<?php 
-    include('config/db_connect.php');
+<?php
 
-    $errors=array('email'=>'', 'title'=>'', 'ingredients'=>'');
+	include('config/db_connect.php');
 
-    if (isset($_POST['submit'])){
-        if (empty($_POST['email'])){
-            $errors['email'] = 'Please Enter An Email! <br />';
-        }
-        elseif (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-            //echo htmlspecialchars($_POST['email']);
+	$email = $title = $ingredients = '';
+	$errors = array('email' => '', 'title' => '', 'ingredients' => '');
 
-            if (empty($_POST['title'])){
-                $errors['title'] = 'Please Enter A Title! <br />';
-            }
-            else {
-                //echo htmlspecialchars($_POST['title']);
-            }
-    
-            if (empty($_POST['ingredients'])){
-                $errors['ingredients'] = 'Please Enter Ingredients! <br />';
-            }
-            else {
-                //echo htmlspecialchars($_POST['ingredients']);
-            }
-        }
-        else {
-            $errors['email'] = 'Please Enter A Valid Email!';
-        }
-    }
+	if(isset($_POST['submit'])){
+		
+		// check email
+		if(empty($_POST['email'])){
+			$errors['email'] = 'An email is required';
+		} else{
+			$email = $_POST['email'];
+			if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+				$errors['email'] = 'Email must be a valid email address';
+			}
+		}
 
-    if(array_filter($errors)){
-        //echo 'errors!'
-    }
-    else{
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $title = mysqli_real_escape_string($conn, $_POST['title']);
-        $ingredients = mysqli_real_escape_string($conn, $_POST['ingredients']);
+		// check title
+		if(empty($_POST['title'])){
+			$errors['title'] = 'A title is required';
+		} else{
+			$title = $_POST['title'];
+			if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+				$errors['title'] = 'Title must be letters and spaces only';
+			}
+		}
 
-        $sql="INSERT INTO pizzas (title,email,ingredients) VALUES ('title', 'email', 'ingredients')";
+		// check ingredients
+		if(empty($_POST['ingredients'])){
+			$errors['ingredients'] = 'At least one ingredient is required';
+		} else{
+			$ingredients = $_POST['ingredients'];
+			if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+				$errors['ingredients'] = 'Ingredients must be a comma separated list';
+			}
+		}
 
-        if(mysqli_query($conn,$sql)){
-            //header('Location: index.php');
-        } else {
-            echo 'error!' . mysqli_error($conn);
-        }
+		if(array_filter($errors)){
+			//echo 'errors in form';
+		} else {
+			// escape sql chars
+			$email = mysqli_real_escape_string($conn, $_POST['email']);
+			$title = mysqli_real_escape_string($conn, $_POST['title']);
+			$ingredients = mysqli_real_escape_string($conn, $_POST['ingredients']);
 
-    }
+			// create sql
+			$sql = "INSERT INTO pizzas(title,email,ingredients) VALUES('$title','$email','$ingredients')";
+
+			// save to db and check
+			if(mysqli_query($conn, $sql)){
+				// success
+				header('Location: index.php');
+			} else {
+				echo 'query error: '. mysqli_error($conn);
+			}
+
+			
+		}
+
+	} // end POST check
+
 ?>
 
 <!DOCTYPE html>
 <html>
-    <?php include('templates/header.php'); ?>
+	
+	<?php include('templates/header.php'); ?>
 
-    <section class="container grey-text">
-        <h4 class="center">
-            Add A Pizza
-        </h4>
-        <form action="add.php" class="white" method="POST">
-            <lable>Your Email:</lable>
-            <input type="text" name="email" >
-            <div class="red-text"><?php echo $errors['email']; ?></div>
-            <lable>Pizza Title:</lable>
-            <input type="text" name="title" >
-            <div class="red-text"><?php echo $errors['title']; ?></div>
-            <lable>Ingredients:</lable>
-            <input type="text" name="ingredients" >
-            <div class="red-text"><?php echo $errors['ingredients']; ?></div>
-            <div class="center">
-                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
-            </div>
-        </form>
-    </section>
+	<section class="container grey-text">
+		<h4 class="center">Add a Pizza</h4>
+		<form class="white" action="add.php" method="POST">
+			<label>Your Email</label>
+			<input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+			<div class="red-text"><?php echo $errors['email']; ?></div>
+			<label>Pizza Title</label>
+			<input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+			<div class="red-text"><?php echo $errors['title']; ?></div>
+			<label>Ingredients (comma separated)</label>
+			<input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+			<div class="red-text"><?php echo $errors['ingredients']; ?></div>
+			<div class="center">
+				<input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
+			</div>
+		</form>
+	</section>
 
-    <?php include('templates/footer.php'); ?>
+	<?php include('templates/footer.php'); ?>
+
 </html>
--- index.php
+++ index.php
@@ -29,7 +29,7 @@
                             <div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
                         </div>
                         <div class="card-action right-align">
-                            <a href="#" class="brand-text">more info</a>
+                            <a href="details.php?id=<?php echo $pizza['id']; ?>" class="brand-text">more info</a>
                         </div>
                     </div>
                 </div>
